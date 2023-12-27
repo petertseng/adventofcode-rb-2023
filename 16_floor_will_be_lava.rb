6ac3b06d019f3247bf59a308c07abc0db151788b
Peter Tseng <pht24@cornell.edu>
2023-12-27 14:06:01 +0000

    day 16: Ractors

--- 16_floor_will_be_lava.rb
+++ 16_floor_will_be_lava.rb
@@ -8,21 +8,21 @@ grid = ARGF.map { |line|
 
 widths = grid.map(&:size)
 raise "inconsistent width #{widths}" if widths.uniq.size != 1
-width = widths[0]
+WIDTH = widths[0]
 height = grid.size
-grid = grid.flatten.freeze
+GRID = grid.flatten.freeze
 
 # any reasonable ordering should make mirrors easy.
 # so let's make splitters easy by making vert/horiz have same parity.
-DIRS = [-width, -1, width, 1].freeze
+DIRS = [-WIDTH, -1, WIDTH, 1].freeze
 UP = 0
 LEFT = 1
 DOWN = 2
 RIGHT = 3
 
-tiles_hit = ->(y0, x0, dir0) {
+def tiles_hit(y0, x0, dir0)
   hit = {}
-  beams = [(y0 * width + x0) << 2 | dir0]
+  beams = [(y0 * WIDTH + x0) << 2 | dir0]
   until beams.empty?
     new_beams = []
     beams.each { |b|
@@ -30,14 +30,14 @@ tiles_hit = ->(y0, x0, dir0) {
       pos = b >> 2
       # grid[pos] is nil if walking off left/right/bottom edge.
       # we would not want to register a hit if so.
-      next unless c = grid[pos]
+      next unless c = GRID[pos]
       # top edge (negative indexing would have returned non-nil)
       next if pos < 0
 
       hit[b] = true
       dir = b & 3
 
-      case c = grid[pos]
+      case c = GRID[pos]
       when :'.'
         new_beams << ((pos + DIRS[dir]) << 2 | dir)
       when :/
@@ -54,8 +54,8 @@ tiles_hit = ->(y0, x0, dir0) {
         if dir & 1 == 0
           new_beams << ((pos + DIRS[dir]) << 2 | dir)
         else
-          new_beams << ((pos - width) << 2 | UP)
-          new_beams << ((pos + width) << 2 | DOWN)
+          new_beams << ((pos - WIDTH) << 2 | UP)
+          new_beams << ((pos + WIDTH) << 2 | DOWN)
         end
       when :-
         if dir & 1 != 0
@@ -64,21 +64,21 @@ tiles_hit = ->(y0, x0, dir0) {
           new_beams << ((pos - 1) << 2 | LEFT)
           new_beams << ((pos + 1) << 2 | RIGHT)
         end
-      else raise "bad #{c} at #{pos.divmod(width)}"
+      else raise "bad #{c} at #{pos.divmod(WIDTH)}"
       end
     }
     beams = new_beams
   end
   hit.keys.map { |u| u >> 2 }.uniq.freeze
-}
+end
 
 pgrid = ->(y0, x0, hits) {
   hits = hits.each_with_index.to_h.freeze
-  grid.each_slice(width).with_index { |row, y|
+  GRID.each_slice(WIDTH).with_index { |row, y|
     puts row.map.with_index { |c, x|
       colour = if y == y0 && x == x0
         '1;31'
-      elsif hits[y * width + x]
+      elsif hits[y * WIDTH + x]
         '1;32'
       else
         0
@@ -89,15 +89,16 @@ pgrid = ->(y0, x0, hits) {
 }
 
 # remember we padded each row with nils to each side, so top-left corner is (0, 1).
-hit1 = tiles_hit[0, 1, RIGHT]
+hit1 = tiles_hit(0, 1, RIGHT)
 puts hit1.size
 pgrid[0, 1, hit1] if verbose
 
-ups    = (1...(width - 1)).map { |x| ((args = [height - 1, x]) + [tiles_hit[*args, UP]]).freeze }
-downs  = (1...(width - 1)).map { |x| ((args = [0, x])          + [tiles_hit[*args, DOWN]]).freeze }
-lefts  = (0...height).map      { |y| ((args = [y, width - 2])  + [tiles_hit[*args, LEFT]]).freeze }
-rights = (0...height).map      { |y| ((args = [y, 1])          + [tiles_hit[*args, RIGHT]]).freeze }
-y0, x0, hit2 = (ups + downs + lefts + rights).max_by { |v| v.last.size }
+ups    = (1...(WIDTH - 1)).map { |x| Ractor.new(height - 1, x) { |*args| (args + [tiles_hit(*args, UP)]).freeze } }
+downs  = (1...(WIDTH - 1)).map { |x| Ractor.new(0, x)          { |*args| (args + [tiles_hit(*args, DOWN)]).freeze } }
+lefts  = (0...height).map      { |y| Ractor.new(y, WIDTH - 2)  { |*args| (args + [tiles_hit(*args, LEFT)]).freeze } }
+rights = (0...height).map      { |y| Ractor.new(y, 1)          { |*args| (args + [tiles_hit(*args, RIGHT)]).freeze } }
+results = (ups + downs + lefts + rights).map(&:take)
+y0, x0, hit2 = results.max_by { |v| v.last.size }
 
 puts hit2.size
 pgrid[y0, x0, hit2] if verbose
